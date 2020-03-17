735cb34a5ca1a4f9de491c0895138d2ad52cbdad
Jerry119 <liu.jerry55@gmail.com>
2020-03-16 23:03:35 -0700

    changed the action bar background color and title text color

--- app/src/main/java/com/andriod/wuziqi/MainActivity.java
+++ app/src/main/java/com/andriod/wuziqi/MainActivity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
@@ -162,6 +163,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
